a9d5f0946ab6f3175170ef09eefaf4a3ad1c64c1
sg788 <sumangtm1083@gmail.com>
2025-01-18 21:53:56 +0630

    -

---
+++ LibraryManagementSystem/Controllers/BorrowingRecordController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorrowingRecordController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BorrowingRecordController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BorrowingRecordList
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BorrowingRecord>>> GetBorrowingRecords()
+        {
+            return await _context.BorrowingRecords.ToListAsync();
+        }
+
+        
+    }
+}
