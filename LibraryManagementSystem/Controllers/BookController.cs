c61fbd6eb74dd0a0c240023dd9c14b1db801835e
sg788 <sumangtm1083@gmail.com>
2025-01-19 21:36:05 +0630

    -

--- LibraryManagementSystem/Controllers/BookController.cs
+++ LibraryManagementSystem/Controllers/BookController.cs
@@ -27,5 +27,20 @@ namespace LibraryManagementSystem.Controllers
             return await _context.Books.ToListAsync();
         }
 
+        // GET: api/Book
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book is null)
+            {
+                return NotFound("No data found");
+            }
+
+            return book;
+        }
+
+        
     }
 }
