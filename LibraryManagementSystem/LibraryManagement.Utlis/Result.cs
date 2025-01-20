edc57bea17ec60b71c9853406134dd7accc85f75
sg788 <sumangtm1083@gmail.com>
2025-01-20 23:15:05 +0630

    feat:Result

--- LibraryManagementSystem/LibraryManagement.Utlis/Result.cs
+++ LibraryManagementSystem/LibraryManagement.Utlis/Result.cs
@@ -1,58 +1,55 @@
 ﻿using LibraryManagementSystem.LibraryManagement.Utlis.Enums;
-using Microsoft.DotNet.Scaffolding.Shared.Messaging;
-using NuGet.Protocol.Plugins;
 
-namespace LibraryManagementSystem.LibraryManagement.Utlis
+namespace LibraryManagementSystem.LibraryManagement.Utlis;
+
+public class Result<T>
 {
-    public class Result<T>
+    public T? Data { get; set; }
+    public bool IsSuccess { get; set; }
+    public EnumHttpStatusCode StatusCode { get; set; }
+    public string? Message { get; set; }
+
+    public static Result<T> Success (string message="Success")
     {
-        public T? Data { get; set; }
-        public bool IsSuccess { get; set; }
-        public EnumHttpStatusCode StatusCode { get; set; }
-        public string? Message { get; set; }
-
-        public static Result<T> Success (string message="Success")
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                Message = message,
-                IsSuccess = true,
-                StatusCode = EnumHttpStatusCode.Success,
-            };
-        }
-
-        public static Result<T> Success (T data, string message="Success")
-        {
-            return new Result<T>
-            {
-                Data = data,
-                IsSuccess = true,
-                StatusCode = EnumHttpStatusCode.Success,
-                Message = message
-            };
-        }
-
-        public static Result<T> Fail(string message="Fail")
-        {
-            return new Result<T>
-            {
-                Message = message,
-                StatusCode = EnumHttpStatusCode.BadRequest,
-                IsSuccess = false
-            };
-        }
-
-        public static Result<T> Fail (Exception ex)
-        {
-            return new Result<T>
-            {
-                Message = ex.Message,
-                StatusCode = EnumHttpStatusCode.InternalServerError,
-                IsSuccess = false
-            };
-        }
-        
+            Message = message,
+            IsSuccess = true,
+            StatusCode = EnumHttpStatusCode.Success,
+        };
     }
 
-   
+    public static Result<T> Success (T data, string message="Success")
+    {
+        return new Result<T>
+        {
+            Data = data,
+            IsSuccess = true,
+            StatusCode = EnumHttpStatusCode.Success,
+            Message = message
+        };
+    }
+
+    public static Result<T> Fail(string message="Fail")
+    {
+        return new Result<T>
+        {
+            Message = message,
+            StatusCode = EnumHttpStatusCode.BadRequest,
+            IsSuccess = false
+        };
+    }
+
+    public static Result<T> Fail (Exception ex)
+    {
+        return new Result<T>
+        {
+            Message = ex.Message,
+            StatusCode = EnumHttpStatusCode.InternalServerError,
+            IsSuccess = false
+        };
+    }
+    
 }
+
+
