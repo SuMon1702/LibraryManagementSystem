cb4b98c8493705e7d0237c6ac8896691037533ff
sg788 <sumangtm1083@gmail.com>
2025-01-20 23:07:09 +0630

    -

---
+++ LibraryManagementSystem/LibraryManagement.Utlis/Result.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis.Enums;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using NuGet.Protocol.Plugins;
+
+namespace LibraryManagementSystem.LibraryManagement.Utlis
+{
+    public class Result<T>
+    {
+        public T? Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public EnumHttpStatusCode StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public static Result<T> Success (string message="Success")
+        {
+            return new Result<T>
+            {
+                Message = message,
+                IsSuccess = true,
+                StatusCode = EnumHttpStatusCode.Success,
+            };
+        }
+    }
+
+   
+}
