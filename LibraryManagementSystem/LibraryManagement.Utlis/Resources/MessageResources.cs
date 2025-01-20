feea3a62a4631e8f1934611786428a104909663a
sg788 <sumangtm1083@gmail.com>
2025-01-20 22:58:46 +0630

    feat:MessageResources

---
+++ LibraryManagementSystem/LibraryManagement.Utlis/Resources/MessageResources.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagementSystem.LibraryManagement.Utlis.Resources
+{
+    public class MessageResources
+    {
+        public static string Success { get; } = "Success";
+        public static string Fail { get; } = "Fail.";
+    }
+}
