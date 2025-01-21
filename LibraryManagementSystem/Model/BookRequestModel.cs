3c6e9cc168b04f49c7e4cb2e2f45444fcd77b3aa
sg788 <sumangtm1083@gmail.com>
2025-01-21 14:22:56 +0630

    feat:BookRequestModel

---
+++ LibraryManagementSystem/Model/BookRequestModel.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryManagementSystem.Model
+{
+    public partial class BookRequestModel
+    {
+        public string BookTitle { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public int BookQty { get; set; }
+
+        public string Publisher { get; set; } = null!;
+
+        public decimal BookPrice { get; set; }
+    }
+}
