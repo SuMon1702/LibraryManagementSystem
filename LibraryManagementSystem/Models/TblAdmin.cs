c1fecb574e08260d025a7a67bda42e07198473c2
sg788 <sumangtm1083@gmail.com>
2025-01-23 11:16:42 +0630

    -

---
+++ LibraryManagementSystem/Models/TblAdmin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementSystem.Models;
+
+public partial class TblAdmin
+{
+    public int AdminId { get; set; }
+
+    public string AdminName { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+}
