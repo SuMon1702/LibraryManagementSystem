6acf04e73840850dc81dab47fdca2490ecf102ef
sg788 <sumangtm1083@gmail.com>
2025-01-23 17:06:37 +0630

    feat:GetMembersAsync

---
+++ LibraryManagementSystem/Controllers/MemberController.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MemberController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Result<List<TblMember>>>> GetMembersAsync(CancellationToken cs)
+        {
+            var member= await _context.TblMembers.ToListAsync(cs);
+            return Result<List<TblMember>>.Success(member);
+        }
+    }
+}
