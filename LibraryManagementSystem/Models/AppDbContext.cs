928cea400dc5c60471950bfaed97963121db7f4e
sg788 <sumangtm1083@gmail.com>
2025-01-23 11:16:29 +0630

    -

--- LibraryManagementSystem/Models/AppDbContext.cs
+++ LibraryManagementSystem/Models/AppDbContext.cs
@@ -15,25 +15,27 @@ public partial class AppDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Admin> Admins { get; set; }
+    public virtual DbSet<TblAdmin> TblAdmins { get; set; }
 
-    public virtual DbSet<Book> Books { get; set; }
+    public virtual DbSet<TblBook> TblBooks { get; set; }
 
-    public virtual DbSet<BorrowingRecord> BorrowingRecords { get; set; }
+    public virtual DbSet<TblBorrowingRecord> TblBorrowingRecords { get; set; }
 
-    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<TblCategory> TblCategories { get; set; }
 
-    public virtual DbSet<Member> Members { get; set; }
+    public virtual DbSet<TblMember> TblMembers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=LibraryManagement;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=.; Database=LibraryManagement; User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Admin>(entity =>
+        modelBuilder.Entity<TblAdmin>(entity =>
         {
-            entity.ToTable("Admin");
+            entity.HasKey(e => e.AdminId).HasName("PK_Admin");
+
+            entity.ToTable("Tbl_Admin");
 
             entity.Property(e => e.AdminId).HasColumnName("AdminID");
             entity.Property(e => e.Address)
@@ -50,15 +52,17 @@ public partial class AppDbContext : DbContext
                 .IsUnicode(false);
         });
 
-        modelBuilder.Entity<Book>(entity =>
+        modelBuilder.Entity<TblBook>(entity =>
         {
-            entity.ToTable("Book");
+            entity.HasKey(e => e.BookId);
+
+            entity.ToTable("Tbl_Book");
 
             entity.Property(e => e.BookId).HasColumnName("BookID");
             entity.Property(e => e.Author)
-                .HasMaxLength(30)
+                .HasMaxLength(50)
                 .IsUnicode(false);
-            entity.Property(e => e.BookPrice).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.BookAmount).HasColumnType("decimal(18, 2)");
             entity.Property(e => e.BookTitle)
                 .HasMaxLength(50)
                 .IsUnicode(false);
@@ -66,32 +70,50 @@ public partial class AppDbContext : DbContext
             entity.Property(e => e.Publisher)
                 .HasMaxLength(50)
                 .IsUnicode(false);
+            entity.Property(e => e.RowVersion)
+                .IsRowVersion()
+                .IsConcurrencyToken();
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false);
         });
 
-        modelBuilder.Entity<BorrowingRecord>(entity =>
+        modelBuilder.Entity<TblBorrowingRecord>(entity =>
         {
-            entity.ToTable("BorrowingRecord");
+            entity.HasKey(e => e.BorrowingRecordId).HasName("PK_BorrowingRecord");
+
+            entity.ToTable("Tbl_BorrowingRecord");
 
             entity.Property(e => e.BorrowingRecordId).HasColumnName("BorrowingRecordID");
             entity.Property(e => e.BookId).HasColumnName("BookID");
             entity.Property(e => e.BookPrice).HasColumnType("decimal(18, 2)");
             entity.Property(e => e.MemberId).HasColumnName("MemberID");
+            entity.Property(e => e.RowVersion)
+                .IsRowVersion()
+                .IsConcurrencyToken();
             entity.Property(e => e.TotalAmount).HasColumnType("decimal(18, 2)");
         });
 
-        modelBuilder.Entity<Category>(entity =>
+        modelBuilder.Entity<TblCategory>(entity =>
         {
-            entity.ToTable("Category");
+            entity.HasKey(e => e.CategoryId).HasName("PK_Category");
+
+            entity.ToTable("Tbl_Category");
 
             entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
             entity.Property(e => e.CategoryName)
                 .HasMaxLength(50)
                 .IsUnicode(false);
+            entity.Property(e => e.RowVersion)
+                .IsRowVersion()
+                .IsConcurrencyToken();
         });
 
-        modelBuilder.Entity<Member>(entity =>
+        modelBuilder.Entity<TblMember>(entity =>
         {
-            entity.ToTable("Member");
+            entity.HasKey(e => e.MemberId).HasName("PK_Member");
+
+            entity.ToTable("Tbl_Member");
 
             entity.Property(e => e.MemberId).HasColumnName("MemberID");
             entity.Property(e => e.Address)
@@ -109,6 +131,9 @@ public partial class AppDbContext : DbContext
             entity.Property(e => e.PhoneNumber)
                 .HasMaxLength(30)
                 .IsUnicode(false);
+            entity.Property(e => e.RowVersion)
+                .IsRowVersion()
+                .IsConcurrencyToken();
         });
 
         OnModelCreatingPartial(modelBuilder);
