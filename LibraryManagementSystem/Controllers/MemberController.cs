d4df9b0b728d29f258c8019af5043cd5fb850593
sg788 <sumangtm1083@gmail.com>
2025-01-24 00:01:56 +0630

    feat:MemberLogin

--- LibraryManagementSystem/Controllers/MemberController.cs
+++ LibraryManagementSystem/Controllers/MemberController.cs
@@ -23,15 +23,15 @@ namespace LibraryManagementSystem.Controllers
         [HttpGet]
         public async Task<ActionResult<Result<List<TblMember>>>> GetMembersAsync(CancellationToken cs)
         {
-            var member= await _context.TblMembers.ToListAsync(cs);
+            var member = await _context.TblMembers.ToListAsync(cs);
             return Result<List<TblMember>>.Success(member);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Result<TblMember>>> GetMemberAsync(int id)
         {
-            var member= await _context.TblCategories.FindAsync(id);
-            if(member is null)
+            var member = await _context.TblCategories.FindAsync(id);
+            if (member is null)
             {
                 return Result<TblMember>.Fail("No member is found");
             }
@@ -41,13 +41,13 @@ namespace LibraryManagementSystem.Controllers
         [HttpPost("Member_Register")]
         public async Task<ActionResult<Result<TblMember>>> MemberRegister(MemberRegisterModel regModel)
         {
-            
-            if(regModel is null)
+
+            if (regModel is null)
             {
                 return Result<TblMember>.Fail("Invalid Member data.");
             }
 
-    //All the data annotations should be filled since they are written [Required], 
+            //All the data annotations should be filled since they are written [Required], 
             if (!ModelState.IsValid)
             {
                 return BadRequest("Please fill all fields");
@@ -68,12 +68,37 @@ namespace LibraryManagementSystem.Controllers
                 ExpireDate = DateTime.Now.AddMonths(6)
             };
 
-           
+
 
             await _context.TblMembers.AddAsync(model);
             await _context.SaveChangesAsync();
 
             return Result<TblMember>.Success(model);
+        }
+
+        [HttpPost("Member_Login")]
+        public async Task<ActionResult<Result<TblMember>>> MemberLogin(MemberLoginModel loginModel)
+        {
+            try
+            {
+                if (loginModel is null)
+                {
+                    return Result<TblMember>.Fail("Invalid Login");
+                }
+
+                var member = await _context.TblMembers.FirstOrDefaultAsync(l => l.Email == loginModel.Email && l.Password == loginModel.Password);
+
+                if (member == null)
+                {
+                    return Result<TblMember>.Fail("Invalid email and password");
+                }
+
+                return Result<TblMember>.Success(member, "Login is succeed");
+            }
+            catch (Exception)
+            {
+                return Result<TblMember>.Fail("An error occurred during member login");
+            }
         }
     }
 }
