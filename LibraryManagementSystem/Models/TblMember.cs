9d647e9949c96501d22adb4c71e9644e4e9fb5a1
sg788 <sumangtm1083@gmail.com>
2025-01-23 11:17:28 +0630

    -

---
+++ LibraryManagementSystem/Models/TblMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementSystem.Models;
+
+public partial class TblMember
+{
+    public int MemberId { get; set; }
+
+    public string MemberName { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public DateOnly MembershipDate { get; set; }
+
+    public string MembershipType { get; set; } = null!;
+
+    public DateOnly ExpireDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public byte[] RowVersion { get; set; } = null!;
+}
