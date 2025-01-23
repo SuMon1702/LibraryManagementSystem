da35e1a2639092b7491249bd148f74b4a1039dcd
sg788 <sumangtm1083@gmail.com>
2025-01-23 12:39:28 +0630

    feat:GetAdminAsync

--- LibraryManagementSystem/Controllers/AdminController.cs
+++ LibraryManagementSystem/Controllers/AdminController.cs
@@ -19,11 +19,24 @@ namespace LibraryManagementSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<Result<List<TblAdmin>>>> GetAdmin()
+        public async Task<ActionResult<Result<List<TblAdmin>>>> GetAdminsAsync()
         {
          
                 var item = await _context.TblAdmins.ToListAsync();
                 return Result<List<TblAdmin>>.Success(item);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Result<TblAdmin>>> GetAdminAsync (int id)
+        {
+            var item = await _context.TblAdmins.FirstOrDefaultAsync(x => x.AdminId == id);
+
+            if (item is null)
+            {
+                return Result<TblAdmin>.Fail("No data is found");
+            }
+
+            return Result<TblAdmin>.Success(item);
         }
     }
 }
