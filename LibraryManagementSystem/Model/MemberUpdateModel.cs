0753d3a1746c3f98ca601f6f47aea8c9f40c37bb
sg788 <sumangtm1083@gmail.com>
2025-01-24 00:16:46 +0630

    -

---
+++ LibraryManagementSystem/Model/MemberUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryManagementSystem.Model
+{
+    public class MemberUpdateModel
+    {
+        public string MemberName { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+        public MemberShipType MembershipType { get; set; } 
+        public string? Password { get; set; }
+    }
+}
