746829f1676dce532b9ae7eae00c7d63724e74b1
sg788 <sumangtm1083@gmail.com>
2025-01-24 00:01:43 +0630

    -

---
+++ LibraryManagementSystem/Model/MemberLoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Model
+{
+    public class MemberLoginModel
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string? Password { get; set; }
+    }
+}
