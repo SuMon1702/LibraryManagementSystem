dcc6c293b40894978a477cb08cd280fbc326d00c
sg788 <sumangtm1083@gmail.com>
2025-01-23 23:46:31 +0630

    [Added]JsonStringEnumConverter

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -4,8 +4,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
