9e69473556e43072534c599043dec98b9aaf7922
sg788 <sumangtm1083@gmail.com>
2025-01-23 23:44:54 +0630

    feat:MemberRegisterModel

---
+++ LibraryManagementSystem/Model/MemberRegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Model
+{
+    public class MemberRegisterModel
+    {
+        [Required]
+        public string MemberName { get; set; } = null!;
+
+        [Required]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public MemberShipType MembershipType { get; set; } 
+    }
+}
