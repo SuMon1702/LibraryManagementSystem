5deab8407754d5aaedcb7f99f231b6c2f0f58569
sg788 <sumangtm1083@gmail.com>
2025-01-24 13:08:33 +0630

    -

---
+++ LibraryManagementSystem/Model/BorrowModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementSystem.Model
+{
+    public class BorrowModel
+    {
+        public int Quantity { get; set; }
+        public int MemberID { get; set; }
+        public int BookID { get; set; }
+    }
+}
