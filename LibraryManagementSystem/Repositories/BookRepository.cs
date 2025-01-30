b5b8842d3f93c4166abbe029612fdfc4843a3e28
sg788 <sumangtm1083@gmail.com>
2025-01-30 08:10:56 +0630

    feat:GetAllBooksAsync

---
+++ LibraryManagementSystem/Repositories/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class BookRepository:IBookRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BookRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TblBook>> GetAllBooksAsync(CancellationToken cs)
+        {
+            return await _context.TblBooks.Where(x => !x.IsActive).ToListAsync(cs);
+        }
+    }
+}
