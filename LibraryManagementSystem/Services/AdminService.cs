fdd29f260fd91923e6149a3203062505d700ef8e
sg788 <sumangtm1083@gmail.com>
2025-01-31 15:46:40 +0630

    -

--- LibraryManagementSystem/Services/AdminService.cs
+++ LibraryManagementSystem/Services/AdminService.cs
@@ -19,7 +19,15 @@ namespace LibraryManagementSystem.Services
             return Result<List<TblAdmin>>.Success(admins);
         } 
 
-       
+        public async Task<Result<TblAdmin>> GetAdminByIdAsync(int id)
+        {
+            var admin = await _adminRepository.GetAdminByIdAsync(id);
+            if (admin == null)
+            {
+                return Result<TblAdmin>.Fail("Admin not found.");
+            }
+            return Result<TblAdmin>.Success(admin);
+        }
     }
 
 }
