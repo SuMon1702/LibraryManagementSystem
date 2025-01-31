4044c9855e526d75d44ea42fb66e82a8788f3bb2
sg788 <sumangtm1083@gmail.com>
2025-01-31 15:40:10 +0630

    -

---
+++ LibraryManagementSystem/Repositories/IAdminRepository.cs
@@ -0,0 +1,10 @@
+﻿using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public interface IAdminRepository
+    {
+        Task<List<TblAdmin>> GetAdminsAsync();
+        
+    }
+}
