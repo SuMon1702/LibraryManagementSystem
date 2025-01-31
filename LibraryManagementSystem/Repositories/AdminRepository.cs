d64306a7deb92598405ecbd177308208176aff53
sg788 <sumangtm1083@gmail.com>
2025-01-31 15:39:55 +0630

    feat:GetAdminsAsync

---
+++ LibraryManagementSystem/Repositories/AdminRepository.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class AdminRepository : IAdminRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AdminRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TblAdmin>> GetAdminsAsync()
+        {
+            return await _context.TblAdmins.ToListAsync();
+        }
+
+        
+    }
+}
