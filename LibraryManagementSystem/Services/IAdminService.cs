5f946d7585a40306e69127ed99f792b2ca513bed
sg788 <sumangtm1083@gmail.com>
2025-01-31 15:44:34 +0630

    -

---
+++ LibraryManagementSystem/Services/IAdminService.cs
@@ -0,0 +1,11 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Services
+{
+    public interface IAdminService
+    {
+        Task<Result<List<TblAdmin>>> GetAdminsAsync();
+        
+    }
+}
