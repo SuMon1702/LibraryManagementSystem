8d0617da6559b9915491915208852ccef671a622
sg788 <sumangtm1083@gmail.com>
2025-01-31 15:44:20 +0630

    -

---
+++ LibraryManagementSystem/Services/AdminService.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories;
+
+namespace LibraryManagementSystem.Services
+{
+    public class AdminService: IAdminService
+    {
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminService(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
+        public async Task<Result<List<TblAdmin>>> GetAdminsAsync()
+        {
+            var admins = await _adminRepository.GetAdminsAsync();
+            return Result<List<TblAdmin>>.Success(admins);
+        } 
+
+       
+    }
+
+}
