4f1031d9bd3461ae4232fc68b1ef85b931dbac93
sg788 <sumangtm1083@gmail.com>
2025-02-02 23:13:12 +0630

    -

---
+++ LibraryManagementSystem/Repositories/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public interface IBookRepository
+    {
+       Task<List<TblBook>> GetAllBooksAsync(CancellationToken cs);
+    }
+}
