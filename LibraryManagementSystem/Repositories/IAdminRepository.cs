6215c4835fc0408056e9d04c2aacd72e24731555
sg788 <sumangtm1083@gmail.com>
2025-02-10 23:11:42 +0630

    -

--- LibraryManagementSystem/Repositories/IAdminRepository.cs
+++ LibraryManagementSystem/Repositories/IAdminRepository.cs
@@ -6,7 +6,7 @@ namespace LibraryManagementSystem.Repositories;
 
 public interface IAdminRepository
 {
-    Task<List<TblAdmin>> GetAdminsAsync();
+    Task<Result<List<TblAdmin>>> GetAdminsAsync();
     Task<Result<TblAdmin?>> GetAdminByIdAsync(int id);
     Task<Result<TblAdmin?>> AdminLogin(string email, string password);
     Task<Result<TblAdmin?>> UpdateAdmin(int id, AdminModel model);
