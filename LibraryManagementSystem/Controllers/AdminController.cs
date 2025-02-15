46db93cbba4fccd0de4a91721d7bc6ffb405f6ce
sg788 <sumangtm1083@gmail.com>
2025-02-15 10:47:19 +0630

    -

--- LibraryManagementSystem/Controllers/AdminController.cs
+++ LibraryManagementSystem/Controllers/AdminController.cs
@@ -1,5 +1,4 @@
 ﻿using LibraryManagementSystem.Dtos;
-using LibraryManagementSystem.LibraryManagement.Utlis;
 using LibraryManagementSystem.Model;
 using LibraryManagementSystem.Models;
 using LibraryManagementSystem.Services;
@@ -10,75 +9,77 @@ using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
 using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
 using HttpPutAttribute = Microsoft.AspNetCore.Mvc.HttpPutAttribute;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
-namespace LibraryManagementSystem.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class AdminController : ControllerBase
+namespace LibraryManagementSystem.Controllers
 {
-
-    private readonly IAdminService _service;
-
-    public AdminController(IAdminService service)
-    {
-        _service = service;
-    }
-
-
-    #region GetAdminsAsync
-    [HttpGet]
-    public async Task<ActionResult<List<TblAdmin>>> GetAdminsAsync()
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
     {
 
-        var result = await _service.GetAdminsAsync();
-        return Ok(result);
-    }
-    #endregion
+        private readonly IAdminService _service;
 
-
-    #region GetAdminAsync
-    [HttpGet("{id}")]
-    public async Task<ActionResult<TblAdmin>> GetAdminAsync(int id)
-    {
-        var result = await _service.GetAdminByIdAsync(id);
-        return Ok(result);
-    }
-    #endregion
-
-
-    #region AdminLogin
-    [HttpPost("admin_Login")]
-    public async Task<ActionResult<TblAdmin>> AdminLogin([FromBody] AdminLoginModel login)
-    {
-        var result = await _service.AdminLogin(login);
-        return Ok(result);
-    }
-    #endregion
-
-
-    #region 
-    [HttpPut("{id}")]
-    public async Task<ActionResult<TblAdmin>> UpdateAdmin(int id, AdminModel model)
-    {
-        if (model is null)
+        public AdminController(IAdminService service)
         {
-            return BadRequest("Invalid request");
+            _service = service;
         }
-        var item = await _service.UpdateAdmin(id, model);
-        return Ok(item);
-    }
-    #endregion
 
-    #region ResetPassword
-    [HttpPut("resetPassword/{adminId}")]
-    public async Task<ActionResult<TblAdmin>> ResetPassword(int adminId, [FromBody] AdminResetModel reset)
-    {
-        if (reset is null || string.IsNullOrWhiteSpace(reset.Password))
+
+        #region GetAdminsAsync
+        [HttpGet]
+        public async Task<ActionResult<List<TblAdmin>>> GetAdminsAsync()
         {
-            return BadRequest("Invalid request. Password cannot be empty.");
+
+            var result = await _service.GetAdminsAsync();
+            return Ok(result);
         }
-        var item = await _service.ResetPassword(adminId, reset.Password);
-        return Ok(item);
+        #endregion
+
+
+        #region GetAdminAsync
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblAdmin>> GetAdminAsync(int id)
+        {
+            var result = await _service.GetAdminByIdAsync(id);
+            return Ok(result);
+        }
+        #endregion
+
+
+        #region AdminLogin
+        [HttpPost("admin_Login")]
+        public async Task<ActionResult<TblAdmin>> AdminLogin([FromBody] AdminLoginModel login)
+        {
+            var result = await _service.AdminLogin(login);
+            return Ok(result);
+        }
+        #endregion
+
+
+        #region 
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TblAdmin>> UpdateAdmin(int id, AdminModel model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Invalid request");
+            }
+            var item = await _service.UpdateAdmin(id, model);
+            return Ok(item);
+        }
+        #endregion
+
+        #region ResetPassword
+        [HttpPut("resetPassword/{adminId}")]
+        public async Task<ActionResult<TblAdmin>> ResetPassword(int adminId, [FromBody] AdminResetModel reset)
+        {
+            if (reset is null || string.IsNullOrWhiteSpace(reset.Password))
+            {
+                return BadRequest("Invalid request. Password cannot be empty.");
+            }
+            var item = await _service.ResetPassword(adminId, reset.Password);
+            return Ok(item);
+        }
+        #endregion
     }
-    #endregion
 }
