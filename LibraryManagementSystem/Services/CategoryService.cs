3a51a61b3e50232427611a68f53b2ac0deccd064
sg788 <sumangtm1083@gmail.com>
2025-02-22 22:24:40 +0630

    -

---
+++ LibraryManagementSystem/Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories;
+
+namespace LibraryManagementSystem.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Result<List<TblCategory>>> GetCategoryAsync()
+        {
+            var category = await _categoryRepository.GetCategoryAsync();
+            return Result<List<TblCategory>>.Success(category.Data!);
+
+        }
+    }
+}
