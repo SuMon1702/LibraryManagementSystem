ffaebacacaf8814a76b3844139e781db54eb117e
sg788 <sumangtm1083@gmail.com>
2025-02-22 22:24:21 +0630

    -

---
+++ LibraryManagementSystem/Repositories/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<List<TblCategory?>>> GetCategoryAsync()
+        {
+            var category = await _context.TblCategories.ToListAsync();
+            return Result<List<TblCategory?>>.Success(category!);
+        }
+    }
+}
