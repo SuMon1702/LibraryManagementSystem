e6264de5792b111a8b5b65c578d340ab06c647e9
sg788 <sumangtm1083@gmail.com>
2025-02-22 22:24:31 +0630

    -

---
+++ LibraryManagementSystem/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Result<List<TblCategory?>>> GetCategoryAsync();
+    }
+}
