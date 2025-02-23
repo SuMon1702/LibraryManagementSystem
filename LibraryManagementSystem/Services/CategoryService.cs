ba87a0d9f1e9a022119f2bfe44402f53ada578f6
sg788 <sumangtm1083@gmail.com>
2025-02-23 20:08:12 +0630

    -

--- LibraryManagementSystem/Services/CategoryService.cs
+++ LibraryManagementSystem/Services/CategoryService.cs
@@ -19,5 +19,11 @@ namespace LibraryManagementSystem.Services
             return Result<List<TblCategory>>.Success(category.Data!);
 
         }
+
+        public async Task<Result<TblCategory>> GetCategoryById(int id)
+        {
+            var category = await _categoryRepository.GetCategoryById(id);
+            return Result<TblCategory>.Success(category.Data!);
+        }
     }
 }
