2db4e70811d4ae3ff4b857f71aea225aad0db0ea
sg788 <sumangtm1083@gmail.com>
2025-02-23 20:08:22 +0630

    -

--- LibraryManagementSystem/Services/ICategoryService.cs
+++ LibraryManagementSystem/Services/ICategoryService.cs
@@ -6,5 +6,6 @@ namespace LibraryManagementSystem.Services
     public interface ICategoryService
     {
         Task<Result<List<TblCategory>>> GetCategoryAsync();
+        Task<Result<TblCategory>> GetCategoryById(int id);
     }
 }
