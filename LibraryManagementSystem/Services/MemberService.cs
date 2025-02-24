e0db0d57ce0fecbe2bb16b89ec8043d9043a07ed
sg788 <sumangtm1083@gmail.com>
2025-02-24 23:26:21 +0630

    -

---
+++ LibraryManagementSystem/Services/MemberService.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories;
+
+namespace LibraryManagementSystem.Services
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IMemberRepository _memberRepository;
+        public MemberService(IMemberRepository memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+        public async Task<Result<List<TblMember?>>> GetMembersAsync()
+        {
+            return await _memberRepository.GetMembersAsync();
+        }
+    }
+}
