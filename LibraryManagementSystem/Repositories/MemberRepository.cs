689c75fafbd39a92533915e41cebf3b1eef51446
sg788 <sumangtm1083@gmail.com>
2025-02-24 23:25:58 +0630

    -

---
+++ LibraryManagementSystem/Repositories/MemberRepository.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class MemberRepository : IMemberRepository
+    {
+        private readonly AppDbContext _context;
+
+        public MemberRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<List<TblMember?>>> GetMembersAsync()
+        {
+            var members = await _context.TblMembers.ToListAsync();
+            return Result<List<TblMember?>>.Success(members!);
+        }
+    }
+}
