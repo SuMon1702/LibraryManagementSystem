4beb8e4fd984ffcb44ffebbc63d852068183968d
sg788 <sumangtm1083@gmail.com>
2025-02-26 23:04:17 +0630

    -

---
+++ LibraryManagementSystem/Repositories/BorrowingRecordRepository.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class BorrowingRecordRepository:IBorrowingRecordRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BorrowingRecordRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<List<TblBorrowingRecord>>> GetBorrowingRecordsAsync()
+        {
+            var borrowingRecords = await _context.TblBorrowingRecords.ToListAsync();
+            return Result<List<TblBorrowingRecord>>.Success(borrowingRecords!);
+        }
+    }
+}
