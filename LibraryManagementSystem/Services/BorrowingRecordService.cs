4e4374675292c4a7b31dc7baf2192f1b4e26b67b
sg788 <sumangtm1083@gmail.com>
2025-02-28 22:44:51 +0630

    -

--- LibraryManagementSystem/Services/BorrowingRecordService.cs
+++ LibraryManagementSystem/Services/BorrowingRecordService.cs
@@ -1,14 +1,20 @@
-﻿using LibraryManagementSystem.Repositories;
+﻿using LibraryManagementSystem.LibraryManagement.Utlis;
+using LibraryManagementSystem.Repositories;
 
 namespace LibraryManagementSystem.Services
 {
-    public class BorrowingRecordService
+    public class BorrowingRecordService : IBorrowingRecordService
     {
         private readonly IBorrowingRecordRepository _borrowingRecordRepository;
 
         public BorrowingRecordService(IBorrowingRecordRepository borrowingRecordRepository)
         {
             _borrowingRecordRepository = borrowingRecordRepository;
+        }
+
+        public async Task<Result<List<TblBorrowingRecord>>> GetBorrowingRecordsAsync()
+        {
+            return await _borrowingRecordRepository.GetBorrowingRecordsAsync();
         }
     }
 }
