a98f13f6e7140d2f578340149103612161d0cc76
sg788 <sumangtm1083@gmail.com>
2025-02-28 22:42:58 +0630

    -

---
+++ LibraryManagementSystem/Services/BorrowingRecordService.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementSystem.Repositories;
+
+namespace LibraryManagementSystem.Services
+{
+    public class BorrowingRecordService
+    {
+        private readonly IBorrowingRecordRepository _borrowingRecordRepository;
+
+        public BorrowingRecordService(IBorrowingRecordRepository borrowingRecordRepository)
+        {
+            _borrowingRecordRepository = borrowingRecordRepository;
+        }
+    }
+}
