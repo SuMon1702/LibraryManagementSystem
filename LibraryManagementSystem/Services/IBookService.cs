c2aad2d5806dc2a9c65c297b3ffe3b856bb8ff1a
sg788 <sumangtm1083@gmail.com>
2025-03-09 22:22:34 +0630

    -

--- LibraryManagementSystem/Services/IBookService.cs
+++ LibraryManagementSystem/Services/IBookService.cs
@@ -1,11 +1,10 @@
 ﻿using LibraryManagementSystem.LibraryManagement.Utlis;
-using LibraryManagementSystem.Model;
-using LibraryManagementSystem.Models;
 
 namespace LibraryManagementSystem.Services
 {
     public interface IBookService
     {
+       
        Task<Result<List<TblBook>>> GetAllBooksAsync();
        Task<Result<TblBook?>> GetBookAsync(int id);
 
