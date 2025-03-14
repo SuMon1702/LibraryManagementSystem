e572aa8a847179d96cb0c562f028a4f65aa488cc
sg788 <sumangtm1083@gmail.com>
2025-03-14 21:20:27 +0630

    -

--- LibraryManagementSystem/Services/IBorrowingRecordService.cs
+++ LibraryManagementSystem/Services/IBorrowingRecordService.cs
@@ -4,7 +4,7 @@ namespace LibraryManagementSystem.Services
 {
     public interface IBorrowingRecordService
     {
-        Task<Result<List<TblBorrowingRecord>>> GetBorrowingRecordsAsync();
+        Task<Result<List<TblBorrowingRecord>>> GetBorrowingRecordsAsync(CancellationToken cs);
         Task<Result<TblBorrowingRecord?>> GetBorrowingRecordsByIdAsync(int id);
 
     }
