2b0dca62a9b952dee27b2dc262a63ffd31683f4c
sg788 <sumangtm1083@gmail.com>
2025-03-19 23:16:39 +0630

    feat:AdminLoginModel

--- LibraryManagementSystem/Dtos/AdminLoginModel.cs
+++ LibraryManagementSystem/Dtos/AdminLoginModel.cs
@@ -1,9 +1,16 @@
-﻿namespace LibraryManagementSystem.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Model
 {
     public class AdminLoginModel
     {
+        
+        [Required]  //Add Data Annotations
+        [EmailAddress] //Add Data Annotations
         public string Email { get; set; } = null!;
 
+        [Required]
+        [MinLength(8)]
         public string Password { get; set; } = null!;
     }
 }
