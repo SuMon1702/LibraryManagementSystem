5d5545df8ca888643367ba47533b0f1f65271c50
sg788 <sumangtm1083@gmail.com>
2025-03-29 22:22:35 +0630

    -

--- LibraryManagementSystem/GlobalUsing.cs
+++ LibraryManagementSystem/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using LibraryManagementSystem.Models;
 global using LibraryManagementSystem.Services;
 global using Microsoft.AspNetCore.Mvc;
+global using LibraryManagementSystem.Repositories.Implementations;
+global using LibraryManagementSystem.Repositories.Interfaces;
+global using Microsoft.EntityFrameworkCore;
