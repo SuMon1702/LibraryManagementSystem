7f594a52ad43f8751d2633bb2ddbc02f2ff3ff12
sg788 <sumangtm1083@gmail.com>
2025-04-16 12:10:26 +0630

    -

--- LibraryManagementSystem/Models/TblMember.cs
+++ LibraryManagementSystem/Models/TblMember.cs
@@ -1,26 +1,27 @@
-﻿namespace LibraryManagementSystem.Models;
-
-public partial class TblMember
+﻿namespace LibraryManagementSystem.Models
 {
-    public int MemberId { get; set; }
+    public partial class TblMember
+    {
+        public int MemberId { get; set; }
 
-    public string MemberName { get; set; } = null!;
+        public string MemberName { get; set; } = null!;
 
-    public string PhoneNumber { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
+        public string Email { get; set; } = null!;
 
-    public string Address { get; set; } = null!;
+        public string Address { get; set; } = null!;
 
-    public DateTime MembershipDate { get; set; }
+        public DateTime MembershipDate { get; set; }
 
-    public string MembershipType { get; set; } = null!;
+        public string MembershipType { get; set; } = null!;
 
-    public DateTime ExpireDate { get; set; }
+        public DateTime ExpireDate { get; set; }
 
-    public bool IsActive { get; set; }
+        public bool IsActive { get; set; }
 
-    public byte[] RowVersion { get; set; } = null!;
+        public byte[] RowVersion { get; set; } = null!;
 
-    public string? Password { get; set; }
+        public string? Password { get; set; }
+    }
 }
