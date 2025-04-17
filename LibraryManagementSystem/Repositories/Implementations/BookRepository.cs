62ca65fcbfd55e982547f8a4694f0b59b43ff5b2
sg788 <sumangtm1083@gmail.com>
2025-04-17 12:01:44 +0630

    -

--- LibraryManagementSystem/Repositories/Implementations/BookRepository.cs
+++ LibraryManagementSystem/Repositories/Implementations/BookRepository.cs
@@ -1,34 +1,33 @@
-﻿namespace LibraryManagementSystem.Repositories.Implementations
+﻿namespace LibraryManagementSystem.Repositories.Implementations;
+
+public class BookRepository : IBookRepository
 {
-    public class BookRepository : IBookRepository
+    private readonly AppDbContext _context;
+
+    public BookRepository(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public BookRepository(AppDbContext context)
+    public async Task<Result<List<TblBook?>>> GetAllBooksAsync(CancellationToken cs)
+    {
+        var item = await _context.TblBooks.ToListAsync(cs);
+
+        if (item is null)
         {
-            _context = context;
-        }
+            return Result<List<TblBook?>>.Fail("No data found.");
 
-        public async Task<Result<List<TblBook?>>> GetAllBooksAsync(CancellationToken cs)
+        }
+        return Result<List<TblBook?>>.Success(item!, "Success");
+    }
+
+    public async Task<Result<TblBook?>> GetBookByIdAsync(int id)
+    {
+        var item = await _context.TblBooks.FirstOrDefaultAsync(x => x.BookId == id);
+        if (item is null)
         {
-            var item = await _context.TblBooks.ToListAsync(cs);
-
-            if (item is null)
-            {
-                return Result<List<TblBook?>>.Fail("No data found.");
-
-            }
-            return Result<List<TblBook?>>.Success(item!, "Success");
+            return Result<TblBook?>.Fail("No data found.");
         }
-
-        public async Task<Result<TblBook?>> GetBookByIdAsync(int id)
-        {
-            var item = await _context.TblBooks.FirstOrDefaultAsync(x => x.BookId == id);
-            if (item is null)
-            {
-                return Result<TblBook?>.Fail("No data found.");
-            }
-            return Result<TblBook?>.Success(item, "Success");
-        }
+        return Result<TblBook?>.Success(item, "Success");
     }
 }
